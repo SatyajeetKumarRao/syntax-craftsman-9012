2de7474f43a5944d8239e5d5d0ebc3099bb4149a
Aman Chaubey <amanchaubey86@gmail.com>
2024-02-23 10:08:11 +0530

    second page dropdowns added

--- frontend/scripts/page2.js
+++ frontend/scripts/page2.js
@@ -1,6 +1,29 @@
+
 function toggleSidebar() {
-    var sidebar = document.getElementById('sidebar');
-    var content = document.getElementById('content');
-    sidebar.classList.toggle('hidden');
-    content.classList.toggle('full-width');
-}
+    const sidebar = document.getElementById('sidebar');
+    const hide_filter_btn = document.querySelector('.hide-filter-btn');
+    sidebar.classList.toggle('hidden-sidebar');
+
+    // Change the text of filter button
+    if (hide_filter_btn.innerText === "Hide Filters") {
+      hide_filter_btn.innerText = "Show Filters";
+    } else {
+      hide_filter_btn.innerText = "Hide Filters";
+    }
+  }
+
+  //dropdown js
+  function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    if (dropdownContent.classList.contains("show")) {
+      dropdownContent.classList.remove("show");
+    } else {
+      dropdownContent.classList.add("show");
+    }
+  }
+  
+  function setRating(rating) {
+    var dropdownContent = document.getElementById("dropdownContent");
+    dropdownContent.classList.remove("show");
+  }
+  
