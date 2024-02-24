8238fcf85c3c2aaedbd47f29fc9deb39ec1a25ee
Satyajeet Kumar Rao <satyajeetrao.rao2@gmail.com>
2024-02-25 04:58:22 +0530

    landing page in progress

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -5,3 +5,99 @@ const footer = createFooter();
 
 // Append footer to the body of the document
 document.body.appendChild(footer);
+
+store_offer_create();
+
+function store_offer_create() {
+
+    let store_offer_array = ['https://www.bigbasket.com/media/uploads/banner_images/hp_topstrip_m_250923_01.png?tr=w-1920,q=80',
+        'https://www.bigbasket.com/media/uploads/banner_images/hp_topstrip_m_250923_02.png?tr=w-1920,q=80',
+        'https://www.bigbasket.com/media/uploads/banner_images/hp_topstrip_m_250923_03.png?tr=w-1920,q=80',
+        'https://www.bigbasket.com/media/uploads/banner_images/hp_topstrip_m_250923_04.png?tr=w-1920,q=80',
+        'https://www.bigbasket.com/media/uploads/banner_images/hp_topstrip_m_250923_05.png?tr=w-1920,q=80',
+        'https://www.bigbasket.com/media/uploads/banner_images/hp_topstrip_m_250923_06.png?tr=w-1920,q=80'];
+
+    store_offer_array = store_offer_array.slice(0, 6);
+
+    let store_offer = document.getElementById('store_offer');
+
+    store_offer_array.map(item => {
+
+        let a = document.createElement('a');
+        a.href = '#';
+
+        let img = document.createElement('img');
+        img.src = item;
+        img.alt = '#';
+        img.loading = 'lazy';
+
+        a.append(img);
+        store_offer.append(a);
+    });
+
+};
+
+bank_offer_create();
+
+function bank_offer_create() {
+
+    let bank_offer_array = ['https://i.ibb.co/N2SjFmB/bank1.webp',
+        'https://i.ibb.co/TWCFrQs/bank2.webp',
+        'https://i.ibb.co/2yfN9ST/bank3.webp',
+        'https://i.ibb.co/J77X4yg/bank4.webp'];
+
+    let bank_offer_img_container = document.getElementById('bank_offer_img_container');
+
+    bank_offer_array.map(item => {
+
+        let a = document.createElement('a');
+        a.href = '#';
+
+        let img = document.createElement('img');
+        img.src = item;
+        img.alt = '#';
+        img.loading = 'lazy';
+
+        a.append(img);
+        bank_offer_img_container.append(a);
+    });
+};
+
+
+top_offer_create();
+
+function top_offer_create() {
+
+    let top_offer_array = ['https://i.ibb.co/2tn3g4w/top1.webp',
+        'https://i.ibb.co/zQwpnmn/top2.webp',
+        'https://i.ibb.co/r0jy3nc/top3.webp',
+        'https://i.ibb.co/cwSShj8/top4.webp'];
+
+    let top_offer_img_container = document.getElementById('top_offer_img_container');
+
+    top_offer_array.map(item => {
+
+        let a = document.createElement('a');
+        a.href = '#';
+
+        let img = document.createElement('img');
+        img.src = item;
+        img.alt = '#';
+        img.loading = 'lazy';
+
+        a.append(img);
+        top_offer_img_container.append(a);
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ frontend/scripts/login.js
@@ -0,0 +1,207 @@
+
+// footer import
+import { createFooter } from '../components/footer.js'
+
+// Generate footer element
+const footer = createFooter();
+
+// Append footer to the body of the document
+document.body.appendChild(footer);
+
+
+// Login SignUp Toggle Script
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+
+// backend server url
+const signInURL = 'http://localhost:4000/login';
+const signUpURL = 'http://localhost:4000/signup';
+
+
+// login operation
+let signin_button = document.getElementById("signin_button");
+
+signin_button.addEventListener("click", () => {
+
+    event.preventDefault();
+    loginUser();
+});
+
+async function loginUser() {
+
+    let login_email = document.getElementById('login_email');
+    let login_password = document.getElementById('login_password');
+
+    let email = login_email.value;
+    let password = login_password.value;
+    if (email && password) {
+
+        let credential = {
+            "email": email,
+            "password": password
+        }
+
+        try {
+            let response = await fetch(signInURL, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(credential),
+            });
+
+            let data = await response.json();
+
+            if (typeof data === 'object') {
+
+                console.log(data);
+                createToast('success');
+
+                localStorage.setItem('userId', data.user.id);
+                localStorage.setItem('localAccessToken', data.accessToken);
+
+                window.location.href = '../index.html';
+            }
+            else {
+                createToast('error');
+            }
+
+        } catch (error) {
+            createToast('error');
+            console.log('Error : Some error occurred', error);
+        }
+    }
+
+    else {
+        createToast('error');
+    }
+}
+
+
+// signup operation
+
+let signup_button = document.getElementById('signup_button');
+
+signup_button.addEventListener('click', () => {
+
+    event.preventDefault();
+    signupUser();
+
+});
+
+
+async function signupUser() {
+
+    let signup_name = document.getElementById('signup_name');
+    let signup_email = document.getElementById('signup_email');
+    let signup_password = document.getElementById('signup_password');
+
+    let name = signup_name.value;
+    let email = signup_email.value;
+    let password = signup_password.value;
+
+    if (name && email && password) {
+
+        let userDetails = {
+            "name": name,
+            "email": email,
+            "password": password,
+            "role": "user"
+        }
+
+        try {
+
+            let response = await fetch(signUpURL, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(userDetails),
+            });
+
+            let data = await response.json();
+
+            if (typeof data === 'object') {
+
+                createToast('success');
+                window.location.href = '../index.html';
+            }
+            else {
+                createToast('error');
+            }
+
+        } catch (error) {
+
+            createToast('error');
+            console.log('Error : Some error occurred', error);
+        }
+
+    }
+    else {
+        createToast('error');
+    }
+
+}
+
+
+
+
+
+
+
+// Toast Notification Script
+const notifications = document.querySelector(".notifications");
+
+const toastDetails = {
+    timer: 5000,
+    success: {
+        icon: 'fa-circle-check',
+        text: 'Success: This is a success toast.',
+    },
+    error: {
+        icon: 'fa-circle-xmark',
+        text: 'Error: This is an error toast.',
+    },
+    warning: {
+        icon: 'fa-triangle-exclamation',
+        text: 'Warning: This is a warning toast.',
+    },
+    info: {
+        icon: 'fa-circle-info',
+        text: 'Info: This is an information toast.',
+    }
+}
+
+const removeToast = (toast_message) => {
+    toast_message.classList.add("hide");
+    if (toast_message.timeoutId) clearTimeout(toast_message.timeoutId); // Clearing the timeout for the toast
+    setTimeout(() => toast_message.remove(), 500); // Removing the toast after 500ms
+}
+
+const createToast = (id) => {
+    // Getting the icon and text for the toast based on the id passed
+    const { icon, text } = toastDetails[id];
+    const toast_message = document.createElement("li"); // Creating a new 'li' element for the toast
+    toast_message.className = `toast_message ${id}`; // Setting the classes for the toast
+    // Setting the inner HTML for the toast
+    toast_message.innerHTML = `<div class="column">
+                         <i class="fa-solid ${icon}"></i>
+                         <span>${text}</span>
+                      </div>
+                      <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
+    notifications.appendChild(toast_message); // Append the toast to the notification ul
+    // Setting a timeout to remove the toast after the specified duration
+    toast_message.timeoutId = setTimeout(() => removeToast(toast_message), toastDetails.timer);
+}
+
+
